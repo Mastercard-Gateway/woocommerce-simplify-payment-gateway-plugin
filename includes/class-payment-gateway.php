b8c5ad639c475ffc5ba1e24fff413820fb1df365
Blain Maguire <maguirbm@tcd.ie>
2021-01-27 12:27:59 +0000

    set a version in user agent the event we get an unexpected exception while trying to fetch it

--- includes/class-payment-gateway.php
+++ includes/class-payment-gateway.php
@@ -206,10 +206,14 @@ class WC_Gateway_Simplify_Commerce extends WC_Payment_Gateway_CC {
 		Simplify::$publicKey  = $this->public_key;
 		Simplify::$privateKey = $this->private_key;
 
-		// try to extract version from main plugin file
-		$plugin_path = dirname( __FILE__ , 2 ) . '/woocommerce-simplify-payment-gateway.php' ;
-		$plugin_data = get_file_data($plugin_path, array('Version' => 'Version'));
-		$plugin_version = $plugin_data['Version'] ?: 'Unknown';
+		try {
+			// try to extract version from main plugin file
+			$plugin_path = dirname( __FILE__ , 2 ) . '/woocommerce-simplify-payment-gateway.php' ;
+			$plugin_data = get_file_data($plugin_path, array('Version' => 'Version'));
+			$plugin_version = $plugin_data['Version'] ?: 'Unknown';
+		} catch ( Exception $e ) {
+			$plugin_version = 'UnknownError';
+		}
 
 		Simplify::$userAgent  = 'SimplifyWooCommercePlugin/' . WC()->version . '/' . $plugin_version;
 	}
